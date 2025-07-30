964dc9b5e9298d3c4f0bd6e0377176391fbdfa5a
Constantine Excel <possigee76@gmail.com>
2025-07-30 11:24:06 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,11 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   "projectId": "farmfresh-connect-km6ym",
   "appId": "1:377386999103:web:be403fa13968b9ad58078a",
@@ -15,7 +12,6 @@ const firebaseConfig = {
   "messagingSenderId": "377386999103"
 };
 
-
 // Initialize Firebase
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 const auth = getAuth(app);
@@ -24,6 +20,5 @@ const googleProvider = new GoogleAuthProvider();
 export const signInWithGoogle = () => {
     return signInWithPopup(auth, googleProvider);
 };
-
 
 export { app, auth };
