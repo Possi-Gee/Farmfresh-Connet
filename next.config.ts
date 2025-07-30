8d194bc606552b9adde47b21ea8e92af9f4f4825
Constantine Excel <possigee76@gmail.com>
2025-07-30 21:59:40 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -46,6 +46,12 @@ const nextConfig: NextConfig = {
         hostname: 'apggreenhouse.com',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'purepng.com',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
